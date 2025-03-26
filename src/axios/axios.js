e36b32f5f4940c7a0df1484fc3ad35d4c33fb6b1
LeoPedroso2323 <leonardopedroso857@gmail.com>
2025-03-26 16:22:50 -0300

    26-03

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/reservas/v1/",
+  baseURL: "http://10.89.240.70:5000/reservas/v1/",
   headers: { accept: "application/json" },
-});
+}); 
 
 const sheets = {
-  postLogin: (usuario) => api.post("login/", usuario),
+  postLogin: (usuario ) => api.post("login/", usuario),
   postCadastro: (usuario) => api.post("user/", usuario),
   getSalas: (sala) => api.get("salas/", sala),
 };
